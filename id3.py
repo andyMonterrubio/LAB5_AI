f732e9f39a12b532dd2d7834203b8d7b38954764
marc <a01200356@itesm.mx>
2017-03-28 23:32:04 -0600

    pep8

--- id3.py
+++ id3.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#! /usr/bin/python
 import fileinput
 from copy import copy
 from math import log
 
 
-#dataset's entropy
+# dataset's entropy
 def entropy(attributes, data):
     total = 0
     rowcount = len(data)
@@ -20,6 +20,7 @@ def entropy(attributes, data):
     return total
 
 
+# information gain based on selected attribute
 def gain(data, entr, attributes, attr_index):
     gain = entr
     rowcount = len(data)
@@ -30,26 +31,25 @@ def gain(data, entr, attributes, attr_index):
             if row[attr_index] == value:
                 count += 1
                 filtered_data.append(row)
-        
-        gain -= (float(count)/rowcount)* entropy(attributes, filtered_data)
-    
+
+        gain -= (float(count)/rowcount) * entropy(attributes, filtered_data)
+
     # print gain
     return gain
 
 
+# create new dataset filtered by an attribute's value
 def filter_data(data, value, attr_index):
     filtered_data = []
-    #print str(attr_index) + ' = ' + value
     for row in data:
         if row[attr_index] == value:
             new_row = copy(row)
-            #print 'row: ' + str(row)
             new_row.pop(attr_index)
             filtered_data.append(new_row)
-    # print filtered_data
     return filtered_data
 
 
+# build decision tree as a dict structure
 def create_id3(attributes, data):
     decision_tree = {}
 
@@ -64,18 +64,18 @@ def create_id3(attributes, data):
         attr_gain = gain(data, entr, attributes, i)
         if attr_gain > maxgain[1]:
             maxgain = (i, attr_gain)
-    
+
     # if there's no more gain, an answer has been reached
     if maxgain[1] == 0:
         for row in data:
             decision_tree['ANSWER'] = row[-1]
             return decision_tree
-    
+
     attr_name = attributes[maxgain[0]]['name']
     new_attributes = copy(attributes)
     new_attributes.pop(maxgain[0])
     # print new_attributes
-    
+
     for value in attributes[maxgain[0]]['values']:
         # print attr_name + ': ' + value
         new_data = filter_data(data, value, maxgain[0])
@@ -86,7 +86,7 @@ def create_id3(attributes, data):
 
 
 # prints tree following the attribute's order
-def print_tree(tree, attributes, identation = 0):
+def print_tree(tree, attributes, identation=0):
     if len(tree) > 1:
         attr_name = tree.keys()[0].split(': ')[0]
         # find attribute used by tree node
@@ -111,32 +111,35 @@ def print_tree(tree, attributes, identation = 0):
         print 'ANSWER: ' + tree.values()[0]
 
 
+# reads input in ARFF format
 if __name__ == "__main__":
     relation = None
     attributes = []
     data = None
-    
+
+    # parse input file
     for line in fileinput.input():
         line = line.replace('\n', '')
         line = line.replace('\r', '')
-        
+
         if line.startswith('%') or not line.strip():
             # ignore comment
             continue
         elif line.startswith('@relation'):
             relation = line[len('@relation') + 1:]
         elif line.startswith('@attribute'):
-            attr_name = line[len('@attribute') + 1:line.find('{')].replace('\t', '').replace(' ', '')
+            attr_name = line[len('@attribute') + 1:line.find('{')]
+            attr_name = attr_name.replace('\t', '').replace(' ', '')
             attr_values = line[line.find('{') + 1: -1].split(', ')
             attributes.append({'name': attr_name, 'values': attr_values})
         elif line.startswith('@data'):
             data = []
         else:
             data.append(line.split(','))
-    
+
     # print relation
     # print attributes
     # print data
-    
+
     decision_tree = create_id3(attributes, data)
     print_tree(decision_tree, attributes)
